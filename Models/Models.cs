5a38752a2a1580374a474a7ea2c285256077af36
tylerbond13 <tyler.robert.bond@gmail.com>
2017-02-13 16:45:49 -0500

    Added models

---
+++ Models/Models.cs
@@ -0,0 +1,18 @@
+using System; 
+namespace FisherInsurance.Models 
+{ 
+    public class Quote 
+    { 
+        public int Id { get; set; } 
+        public string Product { get; set; } 
+        public DateTime ExpireDate { get; set; } 
+        public decimal Price { get; set; } 
+    } 
+     public class Claims 
+    { 
+        public int Id { get; set; } 
+        public string PolicyType { get; set; } 
+        public DateTime LossDate { get; set; } 
+        public decimal LossAmount { get; set; } 
+    } 
+}
